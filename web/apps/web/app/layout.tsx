fbe4fa7ec23f76a817c58c8def1f7de558be5a36
pan <pannanx@gmail.com>
2023-07-28 16:16:10 +0800

    添加 env 配置文件

--- web/apps/web/app/layout.tsx
+++ web/apps/web/app/layout.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
 export default function RootLayout({
                                        children,
                                    }: {
@@ -8,6 +10,7 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body>
+        {baseUrl}
         <ul>
             <li><Link href={'/'}>home</Link></li>
             <li><Link href={'/editor'}>editor</Link></li>
